964b7a739e36e9eafdc6b96fa0cc9b8dd63b095f
Tiago Portes <tiago.fportes@gmail.com>
2026-08-25 16:09:31 -0300

    feat: add 31-calsse-curso.js

--- 06-poo/31-classe-curso.js
+++ 06-poo/31-classe-curso.js
@@ -5,3 +5,20 @@
 //
 // Escreva sua solução abaixo:
 
+class Curso {
+  constructor(nome, duracao, modalidade) {
+    this.nome = nome;
+    this.duracao = duracao;
+    this.modalidade = modalidade;
+  }
+
+  exibirInformacoes() {
+    console.log(`O nome do curso é ${this.nome} | Duração ${this.duracao}hrs`);
+  }
+}
+
+const curso = new Curso("Desenvolvimento de Sistemas", 16, "Presencial");
+curso.exibirInformacoes();
+
+const curso2 = new Curso("Metalurgia", 42, "EAD");
+curso2.exibirInformacoes();
