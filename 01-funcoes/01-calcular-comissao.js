09fc7b7bb873ff07aeee903559b185f8d8a55c4d
Tiago Portes <tiago.fportes@gmail.com>
2026-08-25 11:31:03 -0300

    feat: add 01-caclcular-comissao

--- 01-funcoes/01-calcular-comissao.js
+++ 01-funcoes/01-calcular-comissao.js
@@ -2,8 +2,13 @@
 // Crie uma função chamada calcularComissao que receba o valor de uma venda
 // e a porcentagem de comissão. A função deve retornar o valor da comissão.
 // Teste com uma venda de R$ 800 e comissão de 5%.
-// 
+//
 // Saída esperada: Comissão: R$ 40
 //
 // Escreva sua solução abaixo:
 
+const calcularComissao = (valorComissao, valorVenda) => {
+  return valorVenda * (valorComissao / 100);
+};
+
+console.log(calcularComissao(5, 800));
