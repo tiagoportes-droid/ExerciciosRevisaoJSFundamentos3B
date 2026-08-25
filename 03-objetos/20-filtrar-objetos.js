e498ec694d8ef7abafd1b798ea96c77ae5b39372
Tiago Portes <tiago.fportes@gmail.com>
2026-08-25 14:45:21 -0300

    feat: add 20-filtrar-objetos.js

--- 03-objetos/20-filtrar-objetos.js
+++ 03-objetos/20-filtrar-objetos.js
@@ -5,3 +5,14 @@
 //
 // Escreva sua solução abaixo:
 
+const funcionarios = [
+    {nome: "Tiago", salario: 10000},
+    {nome: "Renan", salario: 1000},
+    {nome: "Matheus", salario: 500},
+    {nome: "João", salario: 3000},
+    {nome: "Ingrid", salario: 9000},
+]
+
+const salario = funcionarios.filter((filtro) => filtro.salario > 3000)
+
+console.log(salario)
